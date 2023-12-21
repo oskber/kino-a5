cf3193979a3913cac60f82541bde5681d09c16db
MickeMarten <KarlMikael.Martensson@gmail.com>
2023-12-21 21:47:40 +0100

    Added array+movie objects to .json

---
+++ main.js
@@ -0,0 +1,8 @@
+        
+//         async function fetchMovies() {
+//         const response = await fetch('./movies.JSON');
+//         const jsonData = await response.json();
+//         console.log(jsonData);
+//     } 
+//   fetchMovies();
+  
