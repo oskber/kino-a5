9383c1dea9fbbdf6b7c222ea80d683d179ec1711
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-01-02 17:16:06 +0100

    Add movie filtering

--- sidebar.js
+++ sidebar.js
@@ -6,16 +6,20 @@ async function init() {
     const sidebarList = document.querySelector(".sidebar__list");
     const searchIcon = document.querySelector(".sidebar__search-icon");
     const searchInput = document.querySelector(".sidebar__search-input");
-    let activeCategory;
+    let activeCategory
 
     await fetch("https://anschoolacc.github.io/Uppgift-2-En-biograf-sajt/movies.JSON")
         .then((res) => res.json())
         .then((data) => {
-            const categories = [...new Set(data.map((movie) => movie.category))];
+            const categories = ["All", ...new Set(data.map((movie) => movie.category))];
             categories.forEach((category) => {
                 const li = document.createElement("li");
                 li.classList.add("sidebar__list-item");
                 li.textContent = capitalize(category);
+                if (category === "All") {
+                    li.classList.add("sidebar__list-item--active");
+                    activeCategory = li;
+                }
                 sidebarList.appendChild(li);
             });
         })
@@ -33,17 +37,36 @@ async function init() {
             searchInput.classList.toggle("sidebar__search-input--open");
             searchInput.focus();
         } else if (e.target.classList.contains("sidebar__list-item")) {
-            activeCategory !== e.target ? activeCategory?.classList.remove("sidebar__list-item--active") : null;
-            e.target.classList.toggle("sidebar__list-item--active");
+            activeCategory.classList.remove('sidebar__list-item--active');
+            e.target.classList.add("sidebar__list-item--active");
             activeCategory = e.target;
+            filterMovies(activeCategory.textContent, searchInput.value);
         }
     });
 
     searchInput.addEventListener("input", (e) => {
-        console.log(e.target.value); // For filtering later
+        filterMovies(activeCategory.textContent, e.target.value);
     });
 }
 
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function filterMovies(activeCategory, searchStr) {
+    const movies = [...document.querySelectorAll(".movieTemplate__card")];
+    const showAll = activeCategory === "All";
+    for(const movie of movies) {
+        const title = movie.querySelector(".movieTemplate__title").textContent.toLowerCase();
+        const category = movie.querySelector(".movieTemplate__category").textContent;
+        if(searchStr && searchStr.length > 2 && !title.includes(searchStr.toLowerCase())) {
+            movie.classList.add("movie--hidden");
+            continue;
+        }
+        if((activeCategory && category === activeCategory) || showAll) {
+            movie.classList.remove("movie--hidden");
+        } else {
+            movie.classList.add("movie--hidden");
+        }
+    }
 }
