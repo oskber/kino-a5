8a8c289e7489965ca1cd4ef7b2845e54c0af0fee
Oskar Bergman <ocke_bergman@hotmail.com>
2024-01-24 23:08:49 +0100

    Got css to work at last. Added styling to newmovies and amovie-page

---
+++ app.js
@@ -0,0 +1,38 @@
+import express, { response } from "express";
+import { engine } from "express-handlebars";
+import fs from "fs/promises";
+import { loadMovie, loadMovies } from "./movies.js";
+
+const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
+
+
+app.get("/", async (request, response) => {
+  response.render('index');
+});
+app.get("/aboutus", async (request, response) => {
+  response.render('aboutUs');
+});
+app.get("/movies", async (request, response) => {
+  response.render("movies");
+});
+app.get("/404", async (request, response) => {
+  response.render("404");
+});
+
+app.get("/newmovies", async (req, res) => {
+  const movies = await loadMovies();
+  res.render("newmovies", { movies });
+});
+
+app.get("/movies/:movieId", async (req, res) => {
+  const movie = await loadMovie(req.params.movieId);
+  res.render("amovie", { movie });
+});
+
+app.use("/static", express.static("./static"));
+
+export default app;
---
+++ tests/movies.spec.js
@@ -0,0 +1,11 @@
+import { test } from "@jest/globals";
+import request from "supertest";
+import app from "../app";
+
+test('Encanto page shows title of movie', async () => {
+  const response = await request(app).get('/movies/2')
+    .expect('Content-Type', 'text/html; charset=utf-8')
+    .expect(200);
+
+  expect(response.text).toMatch('Encanto');
+});
